e5069ee07b0ba62986aac7d2699d8cc143a3b0a6
Johannes Brodwall <johannes@brodwall.com>
2025-01-14 12:38:16 +0100

    install prettier as a test command

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("johannes")).render(<h2>Hello React</h2>)
+createRoot(document.getElementById("johannes")).render(<h2> Hello React</h2>);
